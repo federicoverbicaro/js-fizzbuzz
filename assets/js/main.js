ded521b5b7f035a74faf21f73406959f87a84dcb
federicoverbicaro <federico.verbicaro@icloud.com>
2024-01-09 17:16:10 +0100

    feat:fine Esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,32 @@
 let fizzBuzzQuadrato = document.getElementById("fizzbuzzquadrato")
 
-function fizzbuzzquadrato (){
-
-    let result = ""
-
+function fizzbuzzquadrato() {
     for (let i = 1; i <= 100; i++) {
-        if (i % 3 === 0 && i % 5 === 0){
-            result += '<div class=" fizzbuzzquadrato"><div class="numero">Fizzbuzz</div></div>' 
-        } else if (i % 3 === 0 ){
-            result += '<div class=" fizzbuzzquadrato"><div class="numero">Fizz</div></div>' 
-        }else if (i % 5 === 0){
-            result += '<div class=" fizzbuzzquadrato"><div class="numero">Buzz</div></div>' 
-        }else{
-            result += `<div class=" fizzbuzzquadrato"><div class="numero">${i}</div></div>` 
+
+
+        let container = document.createElement("div");
+        container.classList.add("col-1", "mb-3", "max-width-container", "fizzbuzz-container");
+
+        let numero = document.createElement("div");
+        numero.classList.add("numero");
+
+        if (i % 3 === 0 && i % 5 === 0) {
+            numero.textContent = "Fizzbuzz";
+            numero.style.backgroundColor = "red";
+        } else if (i % 3 === 0) {
+            numero.textContent = "Fizz";
+            numero.style.backgroundColor = "green";
+        } else if (i % 5 === 0) {
+            numero.textContent = "Buzz";
+            numero.style.backgroundColor = "yellow";
+        } else {
+            numero.textContent = i;
+            numero.style.backgroundColor = "blue";
         }
+
+        container.appendChild(numero);
+        fizzBuzzQuadrato.appendChild(container);
     }
-    return result
 }
-fizzBuzzQuadrato.innerHTML = fizzbuzzquadrato()
+
+fizzbuzzquadrato();
